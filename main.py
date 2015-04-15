04fa57b17b363cca1a68dede94f8b42a19192f4a
odoup <odoup@odoup.com>
2015-04-15 20:29:24 +0800

    wgx

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import web
+from web.contrib.template import render_jinja
 
-
-class index:
-
-	def GET(self):
-		return "hello, world"
 
 urls = (
 	'/' , 'index',
 )
+render = render_jinja('templates',encoding='utf-8')
+
+class index:
+
+	def GET(self):
+		return render.index()
+		#return "hello, world"
+
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
