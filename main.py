ec7c7adda5b2dbea3bef1439e31a3244c4ec0e00
odoup <odoup@odoup.com>
2015-04-15 22:59:01 +0800

    url

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from web.contrib.template import render_jinja
 
 urls = (
 	'/' , 'index',
+	'/index', 'index',
+	'/login', 'login',
+	'/register', 'register',
+	'/start', 'start',
+	'/study', 'study',
+	'/guide', 'guide',
 )
 render = render_jinja('templates',encoding='utf-8')
 
@@ -11,6 +17,36 @@ class index:
 
 	def GET(self):
 		return render.index()
+		#return "hello, world"
+
+class login:
+
+	def GET(self):
+		return render.login()
+		#return "hello, world"
+
+class register:
+
+	def GET(self):
+		return render.register()
+		#return "hello, world"
+
+class start:
+
+	def GET(self):
+		return render.start()
+		#return "hello, world"
+
+class study:
+
+	def GET(self):
+		return render.study()
+		#return "hello, world"
+
+class guide:
+
+	def GET(self):
+		return render.guide()
 		#return "hello, world"
 
 
