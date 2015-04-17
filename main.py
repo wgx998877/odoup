33076b12526171f41f87794bb5b2ce78ca25ae48
odoup <odoup@odoup.com>
2015-04-17 11:17:54 +0800

    wgx

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+#coding:utf-8
 import web
 from web.contrib.template import render_jinja
 from model import *
 import json
+import datetime
+from web import form
+web.config.debug = False
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 
 urls = (
 	'/' , 'index',
@@ -11,27 +19,91 @@ urls = (
 	'/start', 'start',
 	'/study', 'study',
 	'/guide', 'guide',
-'/get_question', 'get_question'
+  '/get_question', 'get_question',
+  '/get_userlist', 'get_userlist',
 )
+
+
+app = web.application(urls, globals())
 render = render_jinja('templates',encoding='utf-8')
+session = web.session.Session(app, web.session.DiskStore('sessions'),initializer={'login':0,})
+question = Question()
+user = User(session)
+
 
 class index:
 
-	def GET(self):
+  def GET(self):
 		return render.index()
 		#return "hello, world"
 
 class login:
 
-	def GET(self):
-		return render.login()
-		#return "hello, world"
+  def GET(self):
+    if user.logged():
+      return user.userinfo
+    return render.login()
+
+  def POST(self):
+    postdata = web.input()
+    username = web.net.websafe(postdata.username)
+    password = web.net.websafe(postdata.password)
+    userlist = user.get_user({'username':username})
+    print userlist
+    if userlist is None:
+      return 'no user'
+    u = userlist[0]
+    if u['password'] != password:
+      return 'password error'
+    else:
+      if user.signIn({'username':username,'password':password}):
+        return True
+      return False
+
+class logout:
+  def POST(self):
+    user.logout()
+    return render.logout()
 
 class register:
+  vusername= form.regexp(r".{3,20}$", '用户名长度为3-20位')
+  vpass = form.regexp(r".{6,20}$", '密码长度为6-20位')
+  vemail = form.regexp(r".*@.*", "must be a valid email address")
+  regForm=web.form.Form(
+        form.Textbox('username',vusername,description=u'用户名'),
+        form.Password("password",vpass,description=u"密码"),
+        form.Password("password2", description=u"确认密码"),
+        form.Button(u"马上注册", type="submit", description="submit"),
+        validators = [
+            form.Validator("两次输入的密码不一致", lambda i: i.password == i.password2)]
+        )
+  def GET(self):
+      return render.register()
 
-	def GET(self):
-		return render.register()
-		#return "hello, world"
+  def POST(self):
+      formdata = web.input()
+      username = web.net.websafe(formdata.username)
+      password = web.net.websafe(formdata.password)
+      regip = web.ctx.ip
+      regdate = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+      if not self.regForm.validates():
+        return render.register()
+      else :
+        if user.get_user({'username':username}) is not None:
+          return render.register()
+        else:
+          u = {
+              'username':username,
+              'password':password,
+              'regip'   :regip,
+              'regdate' :regdate,
+          }
+          if user.add_user(u):
+            return web.seeother('/start')
+          else:
+            return u'Register Failed!'
+      return None
+
 
 class start:
 
@@ -52,15 +124,25 @@ class guide:
 		#return "hello, world"
 
 class get_question:
-    def GET(self):
-        query = {}
-        result = question.get_question(query)
-        print result
-        result = json.dumps(result)
-        return result
+  def GET(self):
+    query = {}
+    result = question.get_question(query)
+    result = json.dumps(result)
+    return result
 
-question = Question()
+class get_userlist:
+  def GET(self):
+    query = {}
+    result = user.get_user(query)
+    for i in result:
+      i['_id'] = str(i['_id'])
+    result = {
+        'count' : len(result),
+        'userlist' : result
+        }
+    result = json.dumps(result)
+    return result
+
 
 if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
+  app.run()
--- model.py
+++ model.py
@@ -3,36 +3,167 @@
 
 from pymongo import MongoClient
 import config
+import web
 import logging
+logging.basicConfig()
 
 
-class Question():
-    def __init__(self):
-        self.conn = MongoClient(config.host,config.port)
-        self.db = self.conn.odoup.question
-        self.logger = logging.getLogger()
+class Base(object):
+
+  def __init__(self):
+    self.conn = MongoClient(config.host,config.port).odoup
+    self.logger = logging.getLogger(__name__ + '.log')
+
+class Question(Base):
+
+  def __init__(self):
+    super(Question, self).__init__()
+    self.db = self.conn.question
+
+  def save_question(self, question):
+    if not isinstance(question, dict):
+      return None
+    try:
+      self.db.save(question)
+      return True
+    except:
+      return False
+
+  def get_question(self,query = {}):
+    result = {}
+    if not isinstance(query, dict):
+      return None
+    result['query'] = query
+    try:
+      r = []
+      for i in self.db.find(query):
+        i['_id'] = str(i['_id'])
+        r.append(i)
+      result['result'] = r
+      result['count'] = len(result['result'])
+      result['status'] = config.NORMAL
+    except Exception, e:
+      self.logger.error(e)
+      result['status'] = config.ERROR
+    return result
 
 
-    def get_question(self,query = {}):
-        result = {}
-        if not isinstance(query, dict):
-            return None
-        result['query'] = query
-        try:
-            r = []
-            for i in self.db.find(query):
-                i['_id'] = str(i['_id'])
-                r.append(i)
-            result['result'] = r
-            result['count'] = len(result['result'])
-            result['status'] = config.NORMAL
-        except Exception, e:
-            self.logger.error(e)
-            result['status'] = config.ERROR
-        return result
+
+class User(Base):
+
+  def __init__(self, session = None):
+    super(User, self).__init__()
+    self.db = self.conn.user
+    self.session = session
+    self.userinfo = None
+
+
+  def logged(self):
+    if self.session.login == 1:
+      return True
+    else:
+      return False
+
+  def add_user(self, userinfo):
+    if not isinstance(userinfo, dict):
+      return False
+    try:
+      return self.db.save(userinfo)
+    except:
+      return False
+
+  def del_user(self, userinfo):
+    if not isinstance(userinfo):
+      return False
+    if 'username' not in userinfo or 'password' not in userinfo:
+      return False
+    if not self.has_user(userinfo):
+      return False
+    try:
+      return self.db.remove(userinfo)
+    except:
+      return False
+
+  def update_user(self, query, userinfo):
+    if not isinstance(query):
+      return False
+    if 'username' not in query or 'password' not in query:
+      return False
+    if not self.has_user(query):
+      return False
+    if not isinstance(userinfo):
+      return False
+    if 'username' not in userinfo or 'password' not in userinfo:
+      return False
+    if not self.has_user(userinfo):
+      return False
+    try:
+      return self.db.update(query, {'$unset': userinfo}, False, False, False, True)
+    except:
+      return False
+
+  def get_user(self, userinfo):
+    if not isinstance(userinfo, dict):
+      return None
+    try:
+      result = list(self.db.find(userinfo))
+      return result
+    except:
+      return None
+
+  def has_user(self, userinfo):
+    if not isinstance(userinfo, dict):
+      return False
+    if self.db.find_one(userinfo) is not None:
+      return True
+    else:
+      return False
+
+  def signIn(self, userinfo):
+    if not isinstance(userinfo):
+      return False
+    if 'username' not in userinfo or 'password' not in userinfo:
+      return False
+    if not self.has_user(userinfo):
+      return False
+    try:
+      self.session.login = 1
+      self.userinfo = self.get_user(userinfo)[0]
+      return True
+    except:
+      return False
+
+  def signUp(self, userinfo):
+    if not isinstance(userinfo, dict):
+      return False
+    if self.has_user(userinfo):
+      return False
+    try:
+      self.db.save(userinfo)
+      return True
+    except:
+      return False
+
+  def checkItems(self, keys):
+    key = []
+    for i in keys:
+      if i not in key:
+        return False
+    return True
+
+  def get_current_user(self):
+    if self.session is None or self.session.login != 1:
+      return None
+    return self.userinfo
+
+  def logout(self):
+    self.session.login = 0
+    self.session.kill()
+    self.userinfo = None
+    return True
 
 if __name__ == "__main__":
-    q = Question()
-    r = q.get_question()
-    import json
-    print json.dumps(r)
+  q = Question()
+  r = q.get_question()
+  import json
+  print json.dumps(r)
